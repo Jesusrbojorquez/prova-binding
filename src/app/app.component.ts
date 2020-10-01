9bf5b101972a35af86ef17ea98f8c6e27dcfb917
Jesusrbojorquez <jesusr.bojorquez@pucp.edu.pe>
2020-10-01 11:35:14 +0200

    added files to test bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prova-binding';
+  title = 'test-binding';
+  isValid = false;/**cuando es falso muestra lo que esta dentro de el tag span */
+  textValue = "";
+  shippingAddress="";
+  
+  onClick(){
+    this.isValid = !this.isValid;
+  }
+
+  onInput(event){
+    console.log(event);
+    this.textValue = event.target.value;
+}
 }
